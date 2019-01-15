a1dc72d384ee91a20cc6a619644577157a76e841
hjhyxq2014 <hjhyxq2014@163.com>
2019-01-15 17:30:19 +0800

    记得坐标系变换
    OpenGL自己的坐标系范围 1-，1

--- OpenGLFirstTest/A.cpp
+++ OpenGLFirstTest/A.cpp
@@ -12,14 +12,40 @@
 #include <stdio.h>
 void setPixel(GLfloat x,GLfloat y){
     //用OpenGL自己的函数实现书上的setPixel
-    glPointSize(5.0f);
+    glPointSize(2.0f);
     glBegin(GL_POINTS);
 //    glVertex2i(x,y);
-    glVertex2f(x, y);
+//    glVertex2f(x, y);
+    glVertex3f(x, y, 0);
     glEnd();
     glFlush();
 }
-
+void DDALine(float x0,float y0,float x1,float y1){
+    GLfloat dx,dy,x,y;
+    int deltax,deltay;
+    int steps;
+    deltax = x1 - x0 ;
+    deltay = y1 - y0 ;
+    if (fabs(deltax) > fabs(deltay)) {
+        steps = fabs(deltax);
+    }else {
+        steps = fabs(deltay);
+    }
+    
+    dx = (GLfloat)deltax / (GLfloat)steps;
+    dy = (GLfloat)deltay / (GLfloat)steps;
+    
+    x = x0;
+    y = y0;
+    setPixel(x, y);
+    
+    for (int i = 1; i <= steps; i++) {
+        x += dx;
+        y += dy;
+        setPixel(x/100, y/100);
+    }
+    
+}
 void display(){
     glClear(GL_COLOR_BUFFER_BIT);
 //    glBegin(GL_POLYGON);
@@ -28,19 +54,20 @@ void display(){
 //    glVertex2f(0.5, 0.5);
 //    glVertex2f(0.5, -0.5);
 //    glEnd();
-//
-    setPixel(0, 0);
-    setPixel(0.1, 0.1);
-    setPixel(0.2, 0.2);
+    
+    
+    DDALine(0, 0, 20, 20);
 }
 
 int main(int argc, char** argv){
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-    glutInitWindowSize(500, 500);
-    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(1000, 1000);
+    glutInitWindowPosition(100, 100);
     glutCreateWindow("绘制矩形");
     glutDisplayFunc(display);
     glutMainLoop();
     return 0;
 }
+
+
